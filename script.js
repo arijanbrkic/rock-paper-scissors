5cf285ab55d2600c465a122fd83889c1fe7078a9
arijanbrkic <arijanbrkic1@gmail.com>
2024-02-04 21:37:06 +0100

    Created new function to get random computer selection

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+function getComputerChoice() {
+
+const computerSelection = Math.floor(Math.random() * 3);
+
+switch (computerSelection) {
+    case 0:
+        return 'rock';
+    case 1:
+        return 'paper';
+    case 2:
+        return 'scissors';
+    }
+}
+
+
+
+
+
+
+
+
 function showMessage() {
 
 let userChoice = prompt("Enter your choice (rock, paper, or scissors): ").toUpperCase();
+const computerSelection = getComputerChoice();
 
 if (userChoice === "ROCK") {
     alert("You chose rock!");
@@ -12,4 +34,5 @@ if (userChoice === "ROCK") {
     alert("Choose correctly.")
 }
 
+console.log(computerSelection)
 }
