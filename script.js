f1f189a8b9d2599eaec1fc4f566fb472ed99123a
ArijanBrkic <arijanbrkic1@gmail.com>
2024-09-08 01:40:15 +0200

    Added functions and functionality

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-function playGame(userChoice) {
-    alert(userChoice);
+//declare variables
+let computerChoice;
+let userScore = 0;
+let computerScore = 0;
 
+
+//get computer choice randomly
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
+
+//function to call to update scores
+function updateScores() {
+    document.getElementById('userScore').textContent = userScore;
+    document.getElementById('computerScore').textContent = computerScore;
+}
+
+//delete after testing
+getComputerChoice();
+
+console.log(computerChoice);
+
+//full function to play game
+function playGame(userChoice) {
+    
+    if (userChoice === 'rock' && computerChoice === 'scissors') {
+        userScore++;
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        userScore++;
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        userScore++;
+    } else if (userChoice === computerChoice) {
+        alert("It's a tie!");
+    } else {
+        computerScore++;
+    }
+
+    //update score on webpage
+    updateScores();
+}
+
+//function to reset score and allow user to play again
+function playAgain() {
+    userScore = 0;
+    computerScore = 0;
+
+    updateScores();
 }
